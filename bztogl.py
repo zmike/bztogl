3316f3ca66f4582a1256942747858ee3c085560c
Alberto Ruiz <arclnx@gmail.com>
2017-05-06 12:58:19 +0100

    Process and format bug summary and description

--- bztogl.py
+++ bztogl.py
@@ -21,22 +21,47 @@ import argparse
 import bugzilla
 import gitlab
 
+DESC_TEMPLATE = """*Submitted by {submitter}*
+*Assigned to {asigned_to}*
+*[Link to original bug](https://bugzilla.gnome.org/show_bug.cgi?id={id})*
+
+## Description
+{body}
+"""
+
 class Target:
-    def __init__ (self, token):
+    def __init__ (self, token, product, target_product=None):
         self.token = token
+        self.product = product
+        self.target_product = target_product
 
 class GitLab(Target):
     GITLABURL = "https://gitlab.gnome.org/"
 
-    def __init__ (self, token):
-        Target.__init__(self, token)
+    def __init__ (self, token, product, target_product=None):
+        Target.__init__(self, token, product, target_product)
+        self.gl = None
 
     def connect (self):
         print("Connecting to %s" % self.GITLABURL)
         self.gl = gitlab.Gitlab(self.GITLABURL, self.token)
 
-def initial_comment_to_issue_header(bug, text):
-    pass
+def initial_comment_to_issue_description(bug, text, user_cache):
+    #bzbug.id
+    #bzbug.summary
+    #bzbug.creator
+    #bzbug.creationtime
+    #bzbug.target_milestone
+    #bzbug.blocks
+    #bzbug.assigned_to
+
+    if not text:
+        text = ""
+
+    return DESC_TEMPLATE.format(submitter=user_cache[bug.creator],
+                                asigned_to=bug.assigned_to,
+                                id=bug.id,
+                                body=text)
 
 def attachments_to_comments():
     pass
@@ -44,22 +69,66 @@ def attachments_to_comments():
 def convert_bug_comment_stream():
     pass
 
-def processbug (target, bzbug):
+def populate_user_cache(bgo, target, user_cache):
+    real_names = {}
+    for bzu in bgo.getusers(user_cache.keys()):
+        real_names[bzu.email] = bzu.real_name
+
+    return real_names
+
+def processbug (bgo, target, bzbug):
     print ("Processing bug #%d: %s" % (bzbug.id, bzbug.summary))
     #bzbug.id
     #bzbug.summary
     #bzbug.creator
     #bzbug.creationtime
-
     #bzbug.target_milestone
     #bzbug.blocks
+    #bzbug.assigned_to
 
-    #b1.assigned_to
+    def get_attachments_metadata (self):
+        # pylint: disable=protected-access
+        proxy = self.bugzilla._proxy
+        # pylint: enable=protected-access
 
-    #b1.getcomments
-    #b1.get_attachment_ids()
+        if "attachments" in self.__dict__:
+            attachments = self.attachments
+        else:
+            rawret = proxy.Bug.attachments(
+                {"ids": [self.bug_id], "exclude_fields": ["data"]})
+            attachments = rawret["bugs"][str(self.bug_id)]
+
+        return attachments
+
+    atts = get_attachments_metadata (bzbug)
+    comments = bzbug.getcomments()
+
+    firstcomment = None if len(comments) < 1 else comments[0]
+    desctext = None
+    if firstcomment['author'] == bzbug.creator:
+        desctext = firstcomment['text']
+        comments = comments[1:]
+
+    events = comments + atts
+    events = sorted (events, key=lambda x: x['creation_time'])
+
+    user_cache = {}
+
+    user_cache[bzbug.creator] = None
+    for e in events:
+        user_cache[e['creator']] = None
+
+    user_cache = populate_user_cache (bgo, target, user_cache)
+
+    summary = "[BGO] {}".format(bzbug.summary)
+    description = initial_comment_to_issue_description (bzbug, desctext, user_cache)
+
+    print (summary)
+    print (description)
+
+
     #TODO: Close/close comment
-    pass
+    #TODO: Add ability to resume if something goes wrong (research gitlab/phab metadata)
 
 def options():
     parser = argparse.ArgumentParser(description="Bugzilla migration helper for bugzilla.gnome.org products")
@@ -68,7 +137,7 @@ def options():
     parser.add_argument('--product', help="bugzilla product name", required=True)
     parser.add_argument('--bz-user', help="bugzilla username")
     parser.add_argument('--bz-password', help="bugzilla password")
-
+    parser.add_argument('--target-product', help="product name for the target backend (gitlab/phab)")
     return parser.parse_args()
 
 def main():
@@ -76,7 +145,7 @@ def main():
     args = options()
 
     if args.target == "gitlab":
-        target = GitLab (args.token)
+        target = GitLab (args.token, args.product, args.target_product)
     elif args.target == "phab":
         print("Phabricator target super not implemented")
         sys.exit(1)
@@ -92,9 +161,14 @@ def main():
     query["status"] = ["NEW", "ASSIGNED", "REOPENED", "NEEDINFO", "UNCONFIRMED"]
     print ("Querying for open bugs for the '%s' product" % args.product)
     bzbugs = bgo.query(query)
+    print ("{} bugs found".format(len(bzbugs)))
+    count = 0
 
+    #TODO: Check if there were bugs from this module already filed (i.e. use a tag to mark these)
     for bzbug in bzbugs:
-        processbug (target, bzbug)
+        count += 1
+        sys.stdout.write ('[{}/{}] '.format(count,len(bzbugs)))
+        processbug (bgo, target, bzbug)
 
 if __name__ == '__main__':
     main()
